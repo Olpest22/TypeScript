fdcfddbfe90cff27abceedabfecf5330e47dba0a
Olpest22 <150504553+Olpest22@users.noreply.github.com>
2024-01-17 18:14:03 +1000

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,7 @@
+const number = 1;
+const string = 'str';
+const array = [1,2];
+const func = (ss) => {};
+const c:string[] = [“a”,”b”,”c”]
+const c:any[] = [“a”,”b”,”c”]
+const arrow: () => void = () => {};
